0739c1d28751c06f8f47a0548f745fcda1fcaf82
Kirill Liapustin <asaprokki@gmail.com>
2024-08-18 11:51:20 +0500

    Добавьте файлы проекта.

---
+++ Main.cpp
@@ -0,0 +1,21 @@
+﻿#include "String.hpp"
+#include<iostream>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	String str1 = "Hello World!";
+	str1.print();
+
+	String str2 = str1;
+	str2.print();
+
+	String str3 = String(3, '#');
+
+
+
+
+
+
+	return 0;
+}
